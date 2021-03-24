5274c4ecef064544d1f636506aee5b0396b135b0
edsonDeCarvalho <kedson132@hotmail.com>
2021-03-24 11:14:58 +0100

    changement de initMemory et myalloc selon les indications du prof.

--- PROJET/main.c
+++ PROJET/main.c
@@ -2,64 +2,83 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <memory.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <string.h>
-
-
-char *MAX_ADRESSE;
-int NOMBRE_DE_PAGES;
-int TAILLE_PAGE_OS;
-int *DEJA_ALLUE;
-char *MEMOIRE_INITIALE;
-char *MEMOIRE_ACTUELLE;
-char *REGION_DE_ADRESSE_LIVRE;
-
-
-/**
- * Functions a realizer
- */
-int initMemory(int nBytes);
-void* myalloc(int nBytes);
-void setPagesSize();
-void incrementationMemoire(int incrementation);
-void setPagesSize();
 
 
 
-int main(int argc, char *argv[]) {
-    initMemory(34);
-    int *a=myalloc(sizeof(int));
-    printf("Adresse de a=%p",&a);
-    printf("Ponteur memoire total 1: %p",MEMOIRE_INITIALE);
-    printf("Ponteur memoire incrementeé : %p",MEMOIRE_ACTUELLE);
+char *memoireInitiale;
+char *memoireMax;
+char *memoireActuelle;
+int tailleDePageDeSysteme;
+
+/*Functions Demandées*/
+void initMemory();
+void* myalloc(int nbytes);
+/*Functions utils*/
+int calculTaille(int tailleVariable);
+void setTaileDePageSysteme();
+void setMemoireActuelle(char nouvelleMemoire);
+
+int main(){
+
+    initMemory(50);
+
+    printf("Memoire initiale %p \n",memoireInitiale);
+    printf("Memoire Max %p\n",memoireMax);
+    printf("Memoire Actuelle %p\n",memoireActuelle);
+    int *a;
+    printf("Taille variable %ld\n",sizeof(int));
+    a=(int)myalloc(sizeof(int));
+
+    printf("Adresse variable %p\n",a);
+    printf("Memoire Actuelle %p\n",memoireActuelle);
+
     return 0;
 }
+/**
+ * Function pour recuperer un espace memoire avec
+ * malloc.
+ * @param nbytes
+ */
+void initMemory(int nbytes){
 
-int initMemory(int nBytes){
-    setPagesSize();
-    if(MEMOIRE_INITIALE=mmap(NULL,nBytes,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0)){
-        MEMOIRE_ACTUELLE=MEMOIRE_INITIALE;
-        incrementationMemoire(30);
-        return 0;
+    setTaileDePageSysteme();
+    if((memoireInitiale=(char*)malloc(nbytes))==NULL){
+        fprintf(stderr,"Erreur dans la allocation de la memoire initiale");
+        exit(1);
     }
     else{
-        fprintf(stderr,"Erreur en allocation de debut");
-        return -1;
+        printf("Memoire allueée avec succées \n");
+        memoireMax=memoireInitiale+nbytes;
+        memoireActuelle=memoireInitiale;
     }
 }
+/**
+ * Function pour alluer de la memoire pour une variable dasn le
+ * espace memoire reserveé pour le programme.
+ * @param nBytes
+ * @return
+ */
 void* myalloc(int nBytes){
-    incrementationMemoire(nBytes);
-    return (void *)sbrk(nBytes) ;
-}
-void incrementationMemoire(int incrementation){
-    if(incrementation>0){
-        MEMOIRE_ACTUELLE=MEMOIRE_ACTUELLE+incrementation;
+    int tailleVariable=calculTaille(nBytes);
+    /*Incrementation Memoire*/
+    char *ancienneAdresse=memoireActuelle;
+    if((nBytes<0) || (memoireActuelle>memoireMax)){
+        fprintf(stderr,"Segmente defaoult d'hors de la memoire autorisée");
+        return 0;
     }else{
-        fprintf(stderr,"ERREUR dans l'incrementation de memoire");
+        memoireActuelle=tailleVariable+memoireActuelle;
+        return (void *)ancienneAdresse;
     }
 }
-void setPagesSize(){
-    TAILLE_PAGE_OS=getpagesize();
+/**
+ * Calcul de la talle de la variable.
+ * @param tailleVariable
+ * @return
+ */
+int calculTaille(int tailleVariable){
+    return tailleVariable+tailleDePageDeSysteme & ~0x7;
+}
+
+void setTaileDePageSysteme(){
+    tailleDePageDeSysteme=getpagesize();
 }
